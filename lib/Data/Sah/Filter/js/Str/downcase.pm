6652a93d449aedcdda3366355148014e3ccca852
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-02-10 22:31:38 +0700

    Add filter rules: Str::upcase, Str::downcase

---
+++ lib/Data/Sah/Filter/js/Str/downcase.pm
@@ -0,0 +1,35 @@
+package Data::Sah::Filter::js::Str::downcase;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub meta {
+    +{
+        v => 1,
+        summary => 'Convert string to lowercase',
+        target_type => 'str',
+    };
+}
+
+sub filter {
+    my %args = @_;
+
+    my $dt = $args{data_term};
+
+    my $res = {};
+
+    $res->{expr_filter} = "$dt.toLowerCase()";
+
+    $res;
+}
+
+1;
+# ABSTRACT:
+
+=for Pod::Coverage ^(meta|filter)$
---
+++ lib/Data/Sah/Filter/perl/Str/upcase.pm
@@ -0,0 +1,36 @@
+package Data::Sah::Filter::perl::Str::upcase;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub meta {
+    +{
+        v => 1,
+        summary => 'Convert string to uppercase',
+    };
+}
+
+sub filter {
+    my %args = @_;
+
+    my $dt = $args{data_term};
+
+    my $res = {};
+    $res->{expr_filter} = join(
+        "",
+        "uc($dt)",
+    );
+
+    $res;
+}
+
+1;
+# ABSTRACT:
+
+=for Pod::Coverage ^(meta|filter)$
