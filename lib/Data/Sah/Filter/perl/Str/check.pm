7c0da66b0a71adc2ac60f21d26da3de094c8dbea
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-02-11 17:36:16 +0700

    Add filter: Str::check

---
+++ lib/Data/Sah/Filter/perl/Str/check.pm
@@ -0,0 +1,84 @@
+package Data::Sah::Filter::perl::Str::check;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Data::Dmp;
+
+sub meta {
+    +{
+        v => 1,
+        summary => 'Perform some checks',
+        might_fail => 1,
+        args => {
+            min_len => {
+                schema => 'uint*',
+            },
+            max_len => {
+                schema => 'uint*',
+            },
+            match => {
+                schema => 're*',
+            },
+            in => {
+                schema => ['array*', of=>'str*'],
+            },
+        },
+    };
+}
+
+sub filter {
+    my %fargs = @_;
+
+    my $dt = $fargs{data_term};
+    my $gen_args = $fargs{args} // {};
+
+    my @check_exprs;
+    if (defined $gen_args->{min_len}) {
+        my $val = $gen_args->{min_len} + 0;
+        push @check_exprs, (@check_exprs ? "elsif" : "if") . qq( (length(\$tmp) < $val) { ["Length of data must be at least $val", undef] } );
+    }
+    if (defined $gen_args->{max_len}) {
+        my $val = $gen_args->{max_len} + 0;
+        push @check_exprs, (@check_exprs ? "elsif" : "if") . qq( (length(\$tmp) > $val) { ["Length of data must be at most $val", undef] } );
+    }
+    if (defined $gen_args->{match}) {
+        my $val = ref $gen_args->{match} eq 'Regexp' ? $gen_args->{match} : qr/$gen_args->{match}/;
+        push @check_exprs, (@check_exprs ? "elsif" : "if") . qq| (\$tmp !~ |.dmp($val).qq|) { ["Data must match $val", undef] } |;
+    }
+    if (defined $gen_args->{in}) {
+        my $val = $gen_args->{in};
+        push @check_exprs, (@check_exprs ? "elsif" : "if") . qq| (!grep { \$_ eq \$tmp } \@{ |.dmp($val).qq| }) { ["Data must be one of ".join(", ", \@{|.dmp($val).qq|}), undef] } |;
+    }
+    unless (@check_exprs) {
+        push @check_exprs, qq(if (0) { } );
+    }
+    my $res = {};
+    $res->{expr_filter} = join(
+        "",
+        "do {",
+        "    my \$tmp = $dt; ",
+        @check_exprs,
+        "    else { [undef, \$tmp] } ",
+        "}",
+    );
+
+    $res;
+}
+
+1;
+# ABSTRACT:
+
+=for Pod::Coverage ^(meta|filter)$
+
+=head1 DESCRIPTION
+
+This is more or less a demo filter rule, to show how a filter rule can be used
+to perform some checks. The standard checks performed by this rule, however,
+are better done using standard schema clauses like C<in>, C<min_len>, etc.
